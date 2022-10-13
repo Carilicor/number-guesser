9abe211df9520869a55acc827bdbd2fbbd960f33
Cari Licor <Carilicor@gmail.com>
2022-10-13 09:35:28 -0400

    second

---
+++ guess.js
@@ -0,0 +1,9 @@
+/*
+create a program that guesses a number from 1 to 10
+then ask us to guess that number.
+if it is higher say "too high" and ask again.
+if it's lower say "too low" an ask again.
+if its the right number say "congratulations" and exit.
+*/
+
+import readline from "readline-sync"
