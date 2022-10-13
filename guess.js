2a821597ffd04636cebcbba15c039a60a61f44c7
Cari Licor <Carilicor@gmail.com>
2022-10-13 10:38:21 -0400

    guess code

--- guess.js
+++ guess.js
@@ -7,3 +7,54 @@ if its the right number say "congratulations" and exit.
 */
 
 import readline from "readline-sync"
+
+let userGuessedRight = false
+
+//let NumberChosen = Math.floor(Math.random() *10)
+//console.log(NumberChosen)
+//while(true) { playing - dont use - endless loop
+//random number generator - guess a number 
+let NumberChosen = Math.ceil(Math.random() * 10)
+console.log(NumberChosen)
+//}
+// while (!userGuessedRight) {
+//     let guess = readline.question("What is your guess?")
+//     if (guess == NumberChosen) {
+//         console.log("congratulations")
+//         userGuessedRight = true
+//     }
+// }
+
+
+while (!userGuessedRight) {
+    let guess = readline.question("What is your guess?")
+    if (guess == NumberChosen) {
+
+        console.log("congratulations")
+    }
+        //userGuessedRight = true {
+        if (guess > NumberChosen) {
+            console.log("too high! try again ")
+        }
+        //userGuessedRight = {
+        if (guess < NumberChosen) {
+            console.log("too low! try again ")
+        }
+        //userGuessedRight = true {
+    }
+
+
+
+
+
+
+
+// while (!userGuessedRight) {
+//     let guess = readline.question("What is your guess?")
+//     if (guess == NumberChosen) {
+//         console.log("congratulations")
+//         userGuessedRight = true
+//console.log(guess)to see what i typed - will guess the same number i put
+//while(!userGuessedRight) {
+   //console.log("What is your guess?")
+//}
